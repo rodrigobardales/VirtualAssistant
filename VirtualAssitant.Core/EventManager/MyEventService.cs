783544a0904f60f3e9a3ac67c4e6610d67032718
MarioVelasquezN <rolando.velasquez18@hotmail.com>
2023-05-27 21:51:42 -0600

    arregle mas o menos error de Rodrigo

--- VirtualAssitant.Core/EventManager/MyEventService.cs
+++ VirtualAssitant.Core/EventManager/MyEventService.cs
@@ -9,6 +9,19 @@ namespace VirtualAssitant.Core.EventManager
 {
     public class MyEventService : IMyEventService
     {
-        
+        Task<OperationResult<MyEvent>> IMyEventService.AddEventAsync(string FlightNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<OperationResult<MyEvent>> IMyEventService.DeleteEventAsync(string FlightNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<OperationResult<IReadOnlyList<MyEvent>>> IMyEventService.GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
