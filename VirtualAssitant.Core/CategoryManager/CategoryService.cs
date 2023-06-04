95ff10139b7a99c1f1bfd0595ce35658b41e5899
MarioVelasquezN <rolando.velasquez18@hotmail.com>
2023-06-04 00:47:13 -0600

    categorias Hechas

--- VirtualAssitant.Core/CategoryManager/CategoryService.cs
+++ VirtualAssitant.Core/CategoryManager/CategoryService.cs
@@ -11,12 +11,19 @@ namespace VirtualAssitant.Core.CategoryManager
     {
         public Task<OperationResult<Category>> AddAsync(Category category)
         {
-            throw new NotImplementedException();
+            Category category1 = new Category();
+            category1.CategoryName = category.CategoryName;
+            return Task.FromResult(new OperationResult<Category>(category1));
         }
 
         public Task<OperationResult<IReadOnlyList<Category>>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            Category category1 = new Category();
+            category1.CategoryName = "Category1";
+            return Task.FromResult(new OperationResult<IReadOnlyList<Category>>(new List<Category>() 
+            { 
+                category1 
+            }));
         }
     }
 }
